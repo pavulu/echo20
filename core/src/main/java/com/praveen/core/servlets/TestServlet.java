d9b6b8801d2f0250a37670ce257cddbd02b3dbdf
praveen <pavulu2@gmail.com>
2019-07-25 10:59:16 +0530

    3

--- core/src/main/java/com/praveen/core/servlets/TestServlet.java
+++ core/src/main/java/com/praveen/core/servlets/TestServlet.java
@@ -1,7 +1,11 @@
 package com.praveen.core.servlets;
 
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Component;
 
+import javax.servlet.Servlet;
+
+@Component(service = Servlet.class)
 public class TestServlet extends SlingSafeMethodsServlet {
 
 }
